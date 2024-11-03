4a411c9ff727690629029e5ddd507e74fc7585b3
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-11-03 08:23:20 -0800

    Test commit for the L16 Dialog Assignment

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 // todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const testButton = document.createElement("button");
+testButton.textContent = "Click me!";
+testButton.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+app.appendChild(testButton);
